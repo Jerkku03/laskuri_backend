641e38edcf11b5361da071b08ac290d2e99d05e2
Jerkku03 <108393599+Jerkku03@users.noreply.github.com>
2025-06-10 16:57:22 +0300

    uibuild

--- controllers/data.js
+++ controllers/data.js
@@ -1,5 +1,7 @@
 const dataRouter = require('express').Router()
 
+//https://co2data.fi/api/co2data_construction.json
+
 const data = require('../data.json')
 
 dataRouter.get('/', async (request, response) => {
